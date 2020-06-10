75ae03cd12a7c09e5d4e7d9bc6b79bcc924c58e1
Nikolay <veselovnd@gmail.com>
2020-06-10 17:28:15 +0300

    разбираюсь с джсоном

--- to_json.py
+++ to_json.py
@@ -2,7 +2,7 @@ import json
 import functools
 
 def to_json(func):
-    @functools.wrap
+    @functools.wraps(func)
     def wrapped(*args,**kwargs):
         return json.dumps(func(*args,**kwargs))
     return wrapped
